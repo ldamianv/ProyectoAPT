afde627d304ca6d1c6bca271ba296f28b43fc018
ldamianv <100550638+ldamianv@users.noreply.github.com>
2025-03-07 02:32:26 -0500

    Update SistemaDinamico.js

--- SistemaDinamico.js
+++ SistemaDinamico.js
@@ -4,7 +4,7 @@
       { title: "Traslados Interplantas", desc: "Registre los tiempos de atención de las unidades.", img: "Traslados.jpg", link: "https://docs.google.com/spreadsheets/d/1mnXtHfsSCU5d9nxdB6oosuiX6xkXXoanOVH3no43-YA/edit?usp=sharing" },
       { title: "Control de Film", desc: "Registre el consumo de film en los despachos.", img: "Produccion.jpg", link: "#" },
       { title: "Control de Despachos", desc: "Registre el detalle de los despachos programados.", img: "Ventas.jpeg", link: "#" },
-      { title: "Traslados Granel", desc: "Atención de unidades cargadas con film.", img: "Movimientos.jpeg", link: "#" },
+      { title: "Traslados Granel", desc: "Atención de unidades cargadas con film.", img: "Control.jeg", link: "#" },
       { title: "Reportes de Calidad", desc: "Bultos observados por calidad o daño.", img: "Reporte.jpeg", link: "#" },
       { title: "Exportaciones", desc: "Registra las atenciones de exportaciones.", img: "KPI.jpg", link: "#" }
     ],
