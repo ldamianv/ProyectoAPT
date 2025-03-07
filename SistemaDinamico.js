b17357edc1e7a43fbc389c566de170ba9f5f1bb0
ldamianv <100550638+ldamianv@users.noreply.github.com>
2025-03-07 03:38:43 -0500

    Update SistemaDinamico.js

--- SistemaDinamico.js
+++ SistemaDinamico.js
@@ -2,10 +2,10 @@
   const sectionsData = {
     cp: [
       { title: "Traslados Interplantas", desc: "Registre los tiempos de atención de las unidades.", img: "Traslados.jpeg", link: "https://docs.google.com/spreadsheets/d/1mnXtHfsSCU5d9nxdB6oosuiX6xkXXoanOVH3no43-YA/edit?usp=sharing" },
-      { title: "Control de Film", desc: "Registre el consumo de film en los despachos.", img: "ControlFilm.jpg", link: "#" },
+      { title: "Control de Film", desc: "Registre el consumo de film en los despachos.", img: "ControlFilmx.jpg", link: "#" },
       { title: "Control de Despachos", desc: "Registre el detalle de los despachos programados.", img: "Ventas.jpeg", link: "#" },
-      { title: "Traslados Granel", desc: "Atención de unidades cargadas con film.", img: "Trasladogranel.jpg", link: "#" },
-      { title: "Reportes de Calidad", desc: "Bultos observados por calidad o daño.", img: "Reporte.jpeg", link: "#" },
+      { title: "Traslados Granel", desc: "Atención de unidades cargadas con film.", img: "TrasladoGranelx.jpg", link: "#" },
+      { title: "Reportes de Calidad", desc: "Bultos observados por calidad o daño.", img: "ReporteCalidadx.jpg", link: "#" },
       { title: "Exportaciones", desc: "Registra las atenciones de exportaciones.", img: "Exportacion.jpg", link: "#" }
     ],
     ad: [
