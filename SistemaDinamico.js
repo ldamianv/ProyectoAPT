22fe1ed7161d1701681901f503262c19ceb9bb42
ldamianv <100550638+ldamianv@users.noreply.github.com>
2025-03-20 19:21:34 -0500

    Update SistemaDinamico.js

--- SistemaDinamico.js
+++ SistemaDinamico.js
@@ -20,7 +20,7 @@
     so: [
       { title: "Informe Diario", desc: "Registre los movimientos de productos.", img: "Informex.jpg", link: "#" },
       { title: "Ocupabilidad", desc: "Agrega nuevos productos al inventario.", img: "Ocupabilidadx.jpg", link: "#" },
-      { title: "Horario", desc: "Programa el rol de turnos de tu equipo.", img: "Horariox.jpg", link: "https://docs.google.com/spreadsheets/d/1BDxQgeC_EvCosqG-UfWPszAYeWd6tilewucXXnaZhOM/edit?usp=sharing" }
+      { title: "Horario", desc: "Programa el rol de turnos de tu equipo.", img: "Horariox.jpg", link: "#" }
     ],
     "reportes-generales": [
       { title: "Evolución de Ventas", desc: "Visualiza el progreso de las ventas.", img: "Ventas.jpeg", link: "#" },
