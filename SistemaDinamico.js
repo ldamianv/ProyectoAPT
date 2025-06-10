96cf08eb3e806ddda503195c000427a6d0d48f6c
ldamianv <100550638+ldamianv@users.noreply.github.com>
2025-06-09 23:41:41 -0500

    Update SistemaDinamico.js

--- SistemaDinamico.js
+++ SistemaDinamico.js
@@ -28,8 +28,8 @@
       { title: "Tiempos de Atención", desc: "Consulta los tiempos de atención de unidades.", img: "Traslados.jpeg", link: "https://lookerstudio.google.com/reporting/c65e2dde-10fe-441b-ba64-61a973faad94/page/d55wE/edit" },
       { title: "Descuentos Acumulados", desc: "Consulta los descuentos aplicados.", img: "KPI.jpg", link: "#" },
       { title: "Avance de Ocupabilidad", desc: "Evalúa la ocupación del almacén.", img: "Produccion.jpg", link: "#" },
-      { title: "Reporte de Lento Movimiento", desc: "Identifica productos de lento movimiento.", img: "Movimientosx.jpeg", link: "#" },
-      { title: "Sloting", desc: "Optimiza la ubicación de productos.", img: "Slotingx.jpeg", link: "#" },
+      { title: "Reporte de Lento Movimiento", desc: "Identifica productos de lento movimiento.", img: "Movimientosx.jpg", link: "#" },
+      { title: "Sloting", desc: "Optimiza la ubicación de productos.", img: "Slotingx.jpg", link: "#" },
       { title: "Muestreo de Tarimas", desc: "Visualiza los resultados del muestreo de tarimas.", img: "Traslados.jpeg", link: "#" }
     ],
     birthdays: [
